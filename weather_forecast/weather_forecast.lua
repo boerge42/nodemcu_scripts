7cd86580d449528e0a8250fa1e81a93af6519683
boerge42 <bergeruw@gmx.net>
2016-09-04 17:19:36 +0200

    weather_forecast mit Bildchen

--- weather_forecast/weather_forecast.lua
+++ weather_forecast/weather_forecast.lua
@@ -28,34 +28,47 @@ end
 -- **********************************************************************
 function display_forecast()
     if f ~= nil then
-        disp:firstPage()
-        repeat
-            disp:setFont(u8g.font_9x15)
-            disp:drawStr(0, 10, ""..f[idx].day.."")
-            disp:drawStr(0, 25, ""..f[idx].date.."")
-            disp:drawStr(0, 40, ""..f[idx].low.."/"..f[idx].high.."")
-            disp:setFont(u8g.font_6x10)
-            -- Text eventuell wortweise auf mehrere Zeilen verteilen
-            local x, y = 0, 52
-            for s in string.gmatch(f[idx].text, "%a+") do
-                if (x+string.len(s)*6) > 127 then x, y= 0, y+10 end
-                disp:drawStr(x, y, ""..s.."")
-                x=x+(string.len(s)*6)+6
+            -- Bilddaten laden
+            print(f[idx].code)
+            if file.open(""..f[idx].code..".mono", "r") == nil then
+                file.open("na.mono", "r")
             end
-            -- Tagesposition in der Vorhersage anzeigen
-            local dx = 128/#f
-            x, y = dx/2, 63
-            for i=1, #f, 1 do
-                if i == idx then
-                    disp:drawHLine(x-2, y, 5)
-                else 
-                    disp:drawHLine(x, y, 1)
+            xbm_data = file.read()
+            file.close()
+            disp:firstPage()
+            repeat
+                disp:drawXBM(0, 0, 48, 48, xbm_data)
+                disp:setFont(u8g.font_9x15)
+                disp:drawStr(50, 10, ""..f[idx].day.."")
+                disp:setFont(u8g.font_6x10)
+                disp:drawStr(50, 20, ""..f[idx].date.."")
+                disp:setFont(u8g.font_9x15)
+                disp:drawStr(50, 36, ""..f[idx].low.."/"..f[idx].high.." C")
+                disp:setFont(u8g.font_6x10)
+                -- Text eventuell wortweise auf mehrere Zeilen verteilen
+                --disp:drawStr(x, y, ""..idx.."")
+                x, y= 50, 49
+                for s in string.gmatch(f[idx].text, "%a+") do
+                    if (x+string.len(s)*6) > 128 then x, y= 50, y+10 end
+                    disp:drawStr(x, y, ""..s.."")
+                    --y=y+10
+                    x=x+(string.len(s)*6)+6
                 end
-                x=x+dx
-            end
-        until disp:nextPage() == false
-        idx = idx + 1
-        if idx > #f then idx = 1 end
+                -- Tagesposition in der Vorhersage anzeigen
+                local dx = 128/#f
+                x, y = dx/2, 63
+                for i=1, #f, 1 do
+                    if i == idx then
+                        disp:drawHLine(x-2, y, 5)
+                    else 
+                        disp:drawHLine(x, y, 1)
+                    end
+                    x=x+dx
+                end
+
+            until disp:nextPage() == false
+            idx = idx + 1
+            if idx > #f then idx = 1 end
     end
 end
 
